cc81b1c5edf4c0fe3f920aa4fcc02b9b1584de94
lanamads <128663000+lanamads@users.noreply.github.com>
2024-05-01 12:26:42 +1200

    fix respawn?

--- Assets/Scripts/Menu Scripts/EndMenu.cs
+++ Assets/Scripts/Menu Scripts/EndMenu.cs
@@ -7,12 +7,14 @@ public class EndMenu : MonoBehaviour
 {
     public void ExitToMain()
     {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<Controller>().DeleteGame();
         SceneManager.LoadScene(0);
         //plus logic to reset stats
     }
 
     public void Respawn()
     {
+        GameObject.FindGameObjectWithTag("GameController").GetComponent<Controller>().DeleteGame();
         SceneManager.LoadScene(1);
         //plus logic to reset stats
     }
