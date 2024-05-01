b69ae2432a8dcbea6582195b9947715477c87b5c
lanamads <128663000+lanamads@users.noreply.github.com>
2024-05-01 20:33:18 +1200

    update scripts

--- Assets/Scripts/Menu Scripts/EndMenu.cs
+++ Assets/Scripts/Menu Scripts/EndMenu.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static PersistenceManager;
 
 public class EndMenu : MonoBehaviour
 {
+    public int bossLevel = 5;
+
     public void ExitToMain()
     {
+        StartNewGame();
         SceneManager.LoadScene(0);
         //plus logic to reset stats
     }
 
     public void Respawn()
     {
+        StartNewGame();
         SceneManager.LoadScene(1);
         //plus logic to reset stats
+    }
+
+    private void StartNewGame()
+    {
+        for (int currentLevel = 0; currentLevel < bossLevel; currentLevel++)
+        {
+            string directoryPath = Application.dataPath + "/Saves";
+            string filePath = directoryPath + "/" + currentLevel + "world_state.json";
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+        string levelTrackerPath = Application.dataPath + "/Saves" + "/level_tracker.json";
+        if (File.Exists(levelTrackerPath))
+        {
+            File.Delete(levelTrackerPath);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static PersistenceManager;
 
 public class Player : MonoBehaviour
 {
@@ -11,14 +9,12 @@ public class Player : MonoBehaviour
     public int stamina;
     public int attack;
     public int defense;
-    public int bossLevel = 5;
 
     private void Update()
     {
         if (health <= 0)
         {
             Cursor.lockState = CursorLockMode.None;
-            ResetGame();
             SceneManager.LoadScene(SceneManager.sceneCountInBuildSettings - 1);
         }
     }
@@ -42,24 +38,5 @@ public class Player : MonoBehaviour
     public void resetHealth()
     {
         health = 100;
-    }
-
-    private void ResetGame()
-    {
-        for (int currentLevel = 0; currentLevel < bossLevel; currentLevel++)
-        {
-            string directoryPath = Application.dataPath + "/Saves";
-            string filePath = directoryPath + "/" + currentLevel + "world_state.json";
-
-            if (File.Exists(filePath))
-            {
-                File.Delete(filePath);
-            }
-        }
-        string levelTrackerPath = Application.dataPath + "/Saves" + "/level_tracker.json";
-        if (File.Exists(levelTrackerPath))
-        {
-            File.Delete(levelTrackerPath);
-        }
     }
 }
