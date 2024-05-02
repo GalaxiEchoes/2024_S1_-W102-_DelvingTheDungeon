a5dc832230f344a5009eaf748c444e3066110da7
GalaxiEchoes <144643319+GalaxiEchoes@users.noreply.github.com>
2024-05-02 13:46:34 +1200

    Registration and reset position user stories (#13)
    * login again
    
    * login is finally working
    
    * another part of the login sorted
    
    * the welcome page
    
    * fixed the registration form a little
    
    * login finally sorted phew
    
    * fixed the register bug
    
    * Fixing Merge Conflicts
    
    * Fixing merge issues
    
    * Pulling Main into Alekhya (#12)
    
    * .
    
    * make plane
    
    * menu setup
    
    * fix key input
    
    * create inventory base
    
    * add sprite assets for inventory items
    
    * test
    
    * update
    
    * Implement drag/drop
    
    Given that the player is in the inventory menu, when the player clicks on an item, then they can drag it to another inventory slot.
    
    * fix inventory
    
    * update inventory
    
    * updates
    
    * Ported everything over from local version
    
    Made dungeon generator, dungeon spawner, persistence manager, furniture spawner and a controller to manage it all. Also added some player input interactions early to fix some issues between going between levels.
    
    * .
    
    * Bug Fixes
    
    Working on some bugs in furniture spawning and with the lighting rendering.
    
    * finish inventory
    
    * Fixed a bug with the furniture spawner
    
    fixed torches spawning in the hallway walls on the border.
    
    * Done
    
    Finished and Tested Dungeon Generator
    
    * fix bug
    
    * update view
    
    * Update .gitignore
    
    * Github save file hotfix
    
    * fix bug
    
    * set up game over ui
    
    * switching between scenes
    
    * resolve story conflicts
    
    * fix bugs
    
    * resize box collider
    
    * Fixing link to end menu script
    
    * resize fonts
    
    * fix bug
    
    * fix missing button function
    
    * save script on object please
    
    * reset default view
    
    * readd method on button :(
    
    * Maybe fix script links?
    
    * Add files via upload
    
    * Resolving merge conflicts
    
    * Dungeon generation Bugfix that came from the merge (#7)
    
    * Working on hotfix bug that appeared due to the merge.
    
    * Bug fix for end room spawns
    
    * Fixing Merge Conflicts
    
    * Updating Branch to make hotfix for a bug (#6)
    
    * .
    
    * make plane
    
    * menu setup
    
    * fix key input
    
    * create inventory base
    
    * add sprite assets for inventory items
    
    * test
    
    * update
    
    * Implement drag/drop
    
    Given that the player is in the inventory menu, when the player clicks on an item, then they can drag it to another inventory slot.
    
    * fix inventory
    
    * update inventory
    
    * updates
    
    * .
    
    * finish inventory
    
    * fix bug
    
    * update view
    
    * Update .gitignore
    
    * Github save file hotfix
    
    * fix bug
    
    ---------
    
    Co-authored-by: lanamads <128663000+lanamads@users.noreply.github.com>
    
    * Finally properly fixed the bug that came from merging
    
    ---------
    
    Co-authored-by: lanamads <128663000+lanamads@users.noreply.github.com>
    
    * Update .gitignore
    
    fix meta issues?
    
    * hopefully fixed meta files
    
    * .
    
    * resize box collider
    
    * fix respawn?
    
    * update script
    
    * update respawn
    
    * update script
    
    * update scripts
    
    * update scripts
    
    ---------
    
    Co-authored-by: lanamads <128663000+lanamads@users.noreply.github.com>
    Co-authored-by: GalaxiEchoes <ellenaweissmeyer@gmail.com>
    Co-authored-by: GalaxiEchoes <144643319+GalaxiEchoes@users.noreply.github.com>
    
    * Fixed all the user stories
    
    ---------
    
    Co-authored-by: Alekhya Kondaveeti <nhc1050@autuni.ac.nz>
    Co-authored-by: Alekhya Kondaveeti <116803823+G5-03@users.noreply.github.com>
    Co-authored-by: lanamads <128663000+lanamads@users.noreply.github.com>

---
+++ Assets/Login.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI text;
+    public CanvasManager canvasManager;
+    public TMP_InputField username;
+    public TMP_InputField password;
+    public Button loginButton;
+    public Button goToRegisterButton;
+
+    ArrayList credentials;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginButton.onClick.AddListener(login);
+        goToRegisterButton.onClick.AddListener(moveToRegister);
+
+        if(File.Exists(Application.dataPath + "/credentials.txt"))
+        {
+            credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
+        }
+        else
+        {
+            Debug.Log("Credential file does not exist!");
+        }
+    }
+
+    // Update is called once per frame
+    void login()
+    {
+        bool exists = false;
+        credentials = new ArrayList(File.ReadAllLines(Application.dataPath + "/credentials.txt"));
+
+        foreach(var i in credentials)
+        {
+            string line = i.ToString();
+            if(i.ToString().Substring(0, i.ToString().IndexOf(":")).Equals(username.text)
+                && i.ToString().Substring(i.ToString().IndexOf(":") + 1).Equals(password.text))
+            {
+                exists = true;
+                break;
+            }
+        }
+
+        if(exists)
+        {
+            text.SetText("");
+            Debug.Log($"Logging in '{username.text}'");
+            loadWelcomeScreen();
+        }
+        else
+        {
+            text.SetText("Incorrect credentials!");
+            //Debug.Log("Incorrect credentials!");
+        }
+    }
+
+    void moveToRegister()
+    {
+        canvasManager.OnRegisterButtonClick();
+    }
+
+    void loadWelcomeScreen()
+    {
+        canvasManager.OnMenuButtonClick();
+    }
+}
