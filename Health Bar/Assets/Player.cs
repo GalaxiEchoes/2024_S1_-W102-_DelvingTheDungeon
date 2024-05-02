0555db697c0b72ca8b5b32c1490ac7d0a73dcdc6
RyanQuach2 <rtk5829@autuni.ac.nz>
2024-05-02 14:33:23 +1200

    health bar

--- Health Bar/Assets/Player.cs
+++ Health Bar/Assets/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth = 100;
+    public int currentHealth;
+    public bool dead = false;
+
+    public HealthBar healthBar;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if(currentHealth <= 0)
+        {
+            dead = true;
+        }
         
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 }
