07dc228efc0775499f9f39dedf663d966072b630
RyanQuach2 <rtk5829@autuni.ac.nz>
2024-06-03 13:49:55 +1200

    Cleaning up script

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -7,18 +7,9 @@ using UnityEngine.UI;
 public class EnemyHealth : MonoBehaviour
 { 
     [SerializeField] public Slider slider;
-    [SerializeField] public Camera camera;
-    [SerializeField] public Transform target;
-    [SerializeField] public Vector3 offset;
 
     public void UpdateHealthBar(int currentValue, int maxValue)
     {
         slider.value = currentValue / maxValue;
-    }
-
-    void Update()
-    {
-        transform.rotation = camera.transform.rotation;
-        target.position = target.position + offset;
     }
 }
