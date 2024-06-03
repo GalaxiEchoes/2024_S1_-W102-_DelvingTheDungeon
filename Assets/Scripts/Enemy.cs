37aea95377276334014133d0089e9489765aea3c
Alekhya Kondaveeti <nhc1050@autuni.ac.nz>
2024-06-03 18:47:30 +1200

    audio clip added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class Enemy : MonoBehaviour, IDamageable
 {
     public AudioSource audioSource;
+    public AudioClip deathNoise;
     public AudioClip hitClip;
     int health = 100;
     int playerXP = 10;
     int money = 50;
     
     XPManager xpManager;
-    MoneyTracker moneyTracker;
+    Player player;
 
     public void Start()
     {
         audioSource = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioSource>();
-        Player player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Player>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Player>();
         if(player != null )
         {
             xpManager = player.xpManager;
@@ -42,8 +43,12 @@ public class Enemy : MonoBehaviour, IDamageable
         if(xpManager != null)
         {
             xpManager.addXP(playerXP);
-            moneyTracker.setMoney(money);
         }
+        if(player != null)
+        {
+            player.gainMoney(money);
+        }
+        audioSource.PlayOneShot(deathNoise);
         Destroy(gameObject);
     }
 }
